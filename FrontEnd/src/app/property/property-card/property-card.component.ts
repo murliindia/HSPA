78a9649dda3d81cbdacf90f4cb7ffdc252b7f1c0
Murli <murli.jim@gmail.com>
2022-01-22 19:34:10 +0530

    22.01.22 changes

--- FrontEnd/src/app/property/property-card/property-card.component.ts
+++ FrontEnd/src/app/property/property-card/property-card.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-property-card',
@@ -7,13 +7,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PropertyCardComponent implements OnInit {
 
-  property:any={
-    "ID":1,
-    "Name":"Birla House",
-    "Type":"House",
-    "Price":5000
-  };
-
+ @Input() PropertyData:any;
   constructor() { }
 
   ngOnInit() {
--- FrontEnd/src/app/property/property-list/property-list.component.ts
+++ FrontEnd/src/app/property/property-list/property-list.component.ts
@@ -9,6 +9,65 @@ export class PropertyListComponent implements OnInit {
 
   constructor() { }
 
+  PropertyList:any[]=[
+      {
+      "ID":1,
+      "Name":"Birla House",
+      "Type":"House",
+      "Price":5000
+    },
+    {
+      "ID":2,
+      "Name":"Erose House",
+      "Type":"Villas",
+      "Price":12000
+    },
+    {
+      "ID":3,
+      "Name":"Super House",
+      "Type":"Super",
+      "Price":15000
+    },
+    {
+      "ID":4,
+      "Name":"Super Big House",
+      "Type":"Super Big",
+      "Price":10000
+    },
+    {
+      "ID":5,
+      "Name":"Middle House",
+      "Type":"Middle",
+      "Price":2000
+    },
+    {
+      "ID":6,
+      "Name":"Standard House",
+      "Type":"Standard Vilas",
+      "Price":14000
+    },
+    {
+      "ID":7,
+      "Name":"Deli House",
+      "Type":"Deli",
+      "Price":20000
+    },
+    {
+      "ID":8,
+      "Name":"Noida House",
+      "Type":"Noida",
+      "Price":10000
+    },
+    {
+      "ID":9,
+      "Name":"Gurgaon House",
+      "Type":"Gurgaon",
+      "Price":12000
+    }
+
+];
+
+
   ngOnInit() {
   }
 
